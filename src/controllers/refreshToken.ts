591341619e1ef4d8ae08b8c3d4d80f59742c2c00
Esneider Ospina Oviedo <sneuder8@gmail.com>
2023-03-13 20:33:38 -0500

    feat: verification of oldrefreshtoken

--- src/controllers/refreshToken.ts
+++ src/controllers/refreshToken.ts
@@ -2,19 +2,23 @@ import { Request, Response } from "express";
 import jwt from "jsonwebtoken";
 
 export const refreshTokenController = (req: Request, res: Response) => {
-  const secretWord = process.env.ACCESS_TOKEN_SECRET;
-  const oldRefreshToken = req.headers["refresh-token"];
+  const secretWord = process.env.ACCESS_TOKEN_SECRET || "secret";
+  const oldRefreshToken = req.headers["refresh-token"] as string;
 
-  // token validation
-  if (!oldRefreshToken) return res.status(498).json();
+  try {
+    // token validation with db
+    jwt.verify(oldRefreshToken, secretWord);
 
-  // new pair tokens
-  const newRefreshToken = jwt.sign({}, secretWord, { expiresIn: "48h" });
-  const newAccessToken = jwt.sign({}, secretWord, { expiresIn: "24h" });
+    // new pair tokens
+    const newRefreshToken = jwt.sign({}, secretWord, { expiresIn: "48h" });
+    const newAccessToken = jwt.sign({}, secretWord, { expiresIn: "24h" });
 
-  // send tokens
-  res.header["authorization"] = newAccessToken;
-  res.header["refresh-token"] = newRefreshToken;
+    // send tokens
+    res.set({ authorization: `Bearer ${newAccessToken}` });
+    res.set({ "refresh-token": `${newRefreshToken}` });
 
-  res.json({ message: "token refreshed" });
+    res.json({ message: "token refreshed" });
+  } catch (e) {
+    return res.status(498).json({ message: "no valid token" });
+  }
 };
