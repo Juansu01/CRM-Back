897c6bb0d6e1d6ad803ea890709c0b56661b272c
daochoam <daochoam@unal.edu.co>
2023-04-12 12:41:51 -0500

    Update Note CRUD

---
+++ src/controllers/noteControllers.ts
@@ -0,0 +1,76 @@
+import { Request, Response } from "express";
+import db from "../models";
+import messageGenerator from "../services/messageGenerator";
+
+// export const getOneNote = async (req: Request, res: Response) => {
+//     const { dealId } = parseInt(req.params.dealId);
+//     const OneNote = await db.Note.findOne({ where: { deal_id: dealId } })
+//     return res.status(200).json(OneNote);
+// };
+
+export const getAllNotes = async (req: Request, res: Response) => {
+    const allNote = await db.Note.findAll();
+
+    return res.status(200).json(allNote);
+};
+
+export const getOneNote = async (req: Request, res: Response) => {
+    const dealId = req.params.dealId;
+    await db.Note.findAll({ where: { deal_id: dealId } })
+        .then(note => {
+            if (!note) return res.status(404).end();
+            res.json(note);
+        });
+};
+
+export const createNote = async (req: Request, res: Response) => {
+    const dealId = req.params.dealId;
+    const { content } = req.body;
+    try {
+        const note = await db.Note.findAll({ where: { deal_id: dealId } })
+            .then(note => {
+                if (note.length == 0) {
+                    db.Note.create({
+                        content,
+                        deal_id: parseInt(dealId !== "" ? dealId : null),
+                    })
+                    return res
+                        .status(200)
+                        .json({ message: "Deal created succesfully.", note: content });
+                }
+                else {
+                    res.json(messageGenerator([], "The deal note was previously created"));
+                }
+            })
+    } catch (e) {
+        return res.status(500).json(messageGenerator(["errors", "server"]));
+    }
+};
+
+export const updateNote = async (req: Request, res: Response) => {
+    const dealId = req.params.dealId;
+    const { content } = req.body;
+    try {
+        await db.Note.update({ content: content }, { where: { deal_id: dealId } });
+        return res.json(messageGenerator([], "Note updated succesfully."));
+    } catch (e) {
+        return res.status(500).json(messageGenerator(["errors", "server"]));
+    }
+}
+
+export const deleteNote = async (req: Request, res: Response) => {
+    const dealId = req.params.dealId;
+
+    try {
+        await db.Note.findAll({ where: { deal_id: dealId } }).then(note => {
+            if (note.length !== 0) {
+                db.Note.destroy({ where: { deal_id: dealId } });
+                res.json(messageGenerator([], "Note deleted successfully"));
+            } else {
+                res.json(messageGenerator([], "The deal note has not created"));
+            }
+        })
+    } catch (e) {
+        return res.status(500).json(messageGenerator(["errors", "server"]));
+    }
+};
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,10 +4,12 @@ import funnelRouter from "./funnels";
 import filterRouter from "./filter";
 import leadsRouter from "./leads";
 import dealsRouter from "./deals";
+import notesRouter from "./notes";
 
 export default [
   sessionAndRegistration,
   leadsRouter,
+  notesRouter,
   funnelRouter,
   filterRouter,
   refreshToken,
