93a5ca0e1090800c03ef53d9854120a608e39a86
Juansu01 <thejuank0@hotmail.com>
2023-04-20 16:56:19 -0500

    Added new routes: GET /api/datafiles: Get all DataFiles. GET /api/datafile/:id Get specific DataFile by id. POST /api/datafile Create new DataFile to add to a Deal.

---
+++ src/controllers/dataFileControllers.ts
@@ -0,0 +1,66 @@
+import { Request, Response } from "express";
+import db from "../models";
+import {
+  saveFileLocally,
+  deleteFileFromLocalStorage,
+  uploadFileToCloudinary,
+  deleteFileFromCloudinary,
+} from "../services/uploadToCloudinary";
+
+export const getAllFiles = async (req: Request, res: Response) => {
+  const allFiles = await db.DataFile.findAll();
+
+  return res.status(200).json(allFiles);
+};
+
+export const getFile = async (req: Request, res: Response) => {
+  const id = req.params.id;
+  const file = await db.DataFile.findByPk(id);
+
+  if (file) {
+    return res
+      .status(200)
+      .json({ message: "File successfully found.", file: file });
+  }
+
+  return res.status(404).json({ message: "File wasn't found" });
+};
+
+export const addDataFile = async (req: Request, res: Response) => {
+  const { deal_id } = req.body;
+  const deal = await db.Deal.findByPk(deal_id);
+
+  if (deal) {
+    const fileName = req.file.originalname;
+    saveFileLocally(fileName, req.file.buffer);
+    const result = await uploadFileToCloudinary(fileName).catch((err) => {
+      console.error(err);
+      return res.status(500).json({ message: "Couldn't upload image" });
+    });
+    deleteFileFromLocalStorage(fileName);
+
+    if (result.secure_url) {
+      const newDataFile = await db.DataFile.create({
+        document_file: result.secure_url,
+        deal_id: +deal_id,
+      });
+
+      if (newDataFile) {
+        return res.json({
+          message: "Added new data file successfully",
+          data_file: newDataFile,
+        });
+      }
+      return res.status(500).json({
+        message: "Something went wrong, couldn't create data file instance.",
+      });
+    }
+    return res
+      .status(500)
+      .json({ message: "Something went wrong, couldn't upload file." });
+  }
+
+  return res
+    .status(404)
+    .json({ message: "Deal was not found, cannot add file." });
+};
---
+++ src/routes/datafiles.ts
@@ -0,0 +1,16 @@
+import { Router } from "express";
+import {
+  getAllFiles,
+  getFile,
+  addDataFile,
+} from "../controllers/dataFileControllers";
+import multer from "multer";
+
+const dataFileRouter = Router();
+const upload = multer();
+
+dataFileRouter.get("/datafiles", getAllFiles);
+dataFileRouter.get("/datafile/:id", getFile);
+dataFileRouter.post("/datafile", upload.single("file"), addDataFile);
+
+export default dataFileRouter;
