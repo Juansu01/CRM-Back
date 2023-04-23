8c34fb29ccd9dd9eda6fce6eeae63b25c7e9d260
Juansu01 <thejuank0@hotmail.com>
2023-04-23 14:47:14 -0500

    New Route: PATCH /api/datafile/:id to update file by id

--- src/controllers/dataFileControllers.ts
+++ src/controllers/dataFileControllers.ts
@@ -8,14 +8,14 @@ import {
 } from "../services/uploadToCloudinary";
 
 export const getAllFiles = async (req: Request, res: Response) => {
-  const allFiles = await db.DataFile.findAll();
+  const allFiles = await db.DataFile.findAll({ include: { model: db.Deal } });
 
   return res.status(200).json(allFiles);
 };
 
 export const getFile = async (req: Request, res: Response) => {
   const id = req.params.id;
-  const file = await db.DataFile.findByPk(id);
+  const file = await db.DataFile.findByPk(id, { include: { model: db.Deal } });
 
   if (file) {
     return res
@@ -41,7 +41,7 @@ export const addDataFile = async (req: Request, res: Response) => {
     saveFileLocally(fileName, req.file.buffer);
     const result = await uploadFileToCloudinary(fileName).catch((err) => {
       console.error(err);
-      return res.status(500).json({ message: "Couldn't upload image" });
+      return res.status(500).json({ message: "Couldn't upload file." });
     });
     deleteFileFromLocalStorage(fileName);
 
@@ -73,6 +73,45 @@ export const addDataFile = async (req: Request, res: Response) => {
 
 export const patchDataFile = async (req: Request, res: Response) => {
   const dataFileId = req.params.id;
+  const { deal_id } = req.body;
+  const dataFile = await db.DataFile.findByPk(dataFileId, {
+    include: { model: db.Deal },
+  });
+
+  if (dataFile) {
+    const deal = await db.Deal.findByPk(deal_id);
+    if (deal) {
+      const dealHasDataFile = await deal.getDataFile();
+      if (dealHasDataFile) {
+        return res
+          .status(401)
+          .json({ message: "Deal already has a Datafile." });
+      }
+      dataFile.deal_id = deal_id;
+      await dataFile.save();
+    }
+    if (req.file) {
+      const fileName = req.file.originalname;
+      saveFileLocally(fileName, req.file.buffer);
+      const result = await uploadFileToCloudinary(fileName).catch((err) => {
+        console.error(err);
+        return res.status(500).json({ message: "Couldn't upload file." });
+      });
+      deleteFileFromLocalStorage(fileName);
+      if (result.secure_url) {
+        const previousFileLink = dataFile.document_file;
+        dataFile.document_file = result.secure_url;
+        await dataFile.save();
+        deleteFileFromCloudinary(previousFileLink);
+        return res.json({
+          message: "Datafile successfully updated.",
+          data_file: dataFile,
+        });
+      }
+    }
+    return res.json({ message: "Successfully linked Datafile with Deal." });
+  }
+  return res.status(404).json({ message: "Datafile was not found." });
 };
 
 export const deleteDataFile = async (req: Request, res: Response) => {
--- src/routes/datafiles.ts
+++ src/routes/datafiles.ts
@@ -4,6 +4,7 @@ import {
   getFile,
   addDataFile,
   deleteDataFile,
+  patchDataFile,
 } from "../controllers/dataFileControllers";
 import multer from "multer";
 
@@ -13,6 +14,7 @@ const upload = multer();
 dataFileRouter.get("/datafiles", getAllFiles);
 dataFileRouter.get("/datafile/:id", getFile);
 dataFileRouter.post("/datafile", upload.single("file"), addDataFile);
+dataFileRouter.patch("/datafile/:id", upload.single("file"), patchDataFile);
 dataFileRouter.delete("/datafile/:id", deleteDataFile);
 
 export default dataFileRouter;
