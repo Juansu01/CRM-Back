abc6faa190adea4ef41a91f5a0fa7380f2e4735d
Juansu01 <thejuank0@hotmail.com>
2023-05-06 13:56:01 -0500

    Refactor middleware

--- src/middlewares/checkAccessToken.ts
+++ src/middlewares/checkAccessToken.ts
@@ -7,32 +7,44 @@ const checkAccessToken = async (
   res: Response,
   next: NextFunction
 ) => {
-  const accessToken = req.body.accessToken;
-  const userEmail = req.body.email;
+  const accessToken = req.body.access_token;
+  const userEmail = req.body.user_email;
 
-  jwt.verify(
-    accessToken,
-    process.env.ACCESS_TOKEN_SECRET,
-    async (err, decoded) => {
-      if (err) {
-        if (err.message === "invalid signature")
-          return res.status(401).json({ message: "Invalid token" });
-        if (err.message === "jwt expired") {
-          const result = await generateAccessTokenUsingRefreshToken(userEmail);
-          if (result) {
-            console.log("New access token generated");
-            // Here you set the new token inside cookie
-            next();
-          }
-          if (!result)
-            return res
-              .status(409)
-              .json({ message: "Refresh token expired, please log in again." });
-        }
-      }
-      next();
+  if (!userEmail)
+    return res
+      .status(401)
+      .json({ message: "Please provide an email for user authentication." });
+
+  if (!accessToken)
+    return res.status(401).json({ message: "Please provide an access token." });
+
+  try {
+    const decoded = jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET);
+
+    // Access token is valid, proceed to the next middleware
+    next();
+  } catch (err) {
+    if (err.message === "invalid signature") {
+      return res.status(401).json({ message: "Invalid access token." });
     }
-  );
+    if (err.message === "jwt expired") {
+      try {
+        const result = await generateAccessTokenUsingRefreshToken(userEmail);
+        if (result) {
+          console.log("New access token generated.");
+          // Here you set the new token inside a cookie or response header
+          next();
+        } else {
+          return res
+            .status(409)
+            .json({ message: "Refresh token expired, please log in again." });
+        }
+      } catch (err) {
+        // Handle any errors that occur during token generation or retrieval
+        return res.status(500).json({ message: "Internal server error" });
+      }
+    }
+  }
 };
 
 export default checkAccessToken;
